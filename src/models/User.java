4944377cd6ffbbb586fea9306f7a68784e672ddb
lehom <lehom@DESKTOP-P5U8BU7>
2016-09-29 14:51:28 +0100

    Ready lab TDD-02

--- src/models/User.java
+++ src/models/User.java
@@ -1,9 +1,12 @@
 
 package models;
+import com.google.common.base.Objects;
 import static com.google.common.base.MoreObjects.toStringHelper;
 public class User {
 
- 
+	  static Long   counter = 0l;
+
+	  public Long   id;
 	
 	  public String firstName;
 	  public String lastName;
@@ -16,17 +19,25 @@ public class User {
 
 	  public User(String firstName, String lastName, String email, String password)
 	  {
+		this.id        = counter++;
 	    this.firstName = firstName;
 	    this.lastName = lastName;
 	    this.email = email;
 	    this.password = password;
 	  }
+	  @Override
 	  public String toString()
 	  {
-	    return toStringHelper(this).addValue(firstName)
+	    return toStringHelper(this).addValue(id)
+	                               .addValue(firstName)
 	                               .addValue(lastName)
 	                               .addValue(password)
 	                               .addValue(email)                               
 	                               .toString();
 	  }
+	  @Override  
+	  public int hashCode()  
+	  {  
+	     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
+	  }  
 	}
