a2f287989cbd1c989c9ffd0a5890fbbb210a90fb
lehom <lehom@DESKTOP-P5U8BU7>
2016-10-05 21:55:25 +0100

    TDD-03 All Tests done

---
+++ test/models/LocationTest.java
@@ -0,0 +1,82 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static models.Fixtures.locations;
+
+public class LocationTest
+{ 
+  @Test
+  public void testCreate()
+  {
+    assertEquals (0.01, 23.3f, locations[0].latitude);
+    assertEquals (0.01, 33.3f, locations[0].longitude);
+  }
+
+  @Test
+  public void testIds()
+  {
+    assertNotEquals(locations[0].id, locations[1].id);
+  }
+
+  @Test
+  public void testToString()
+  {
+    assertEquals ("Location{" + locations[0].id + ", 23.3, 33.3}", locations[0].toString());
+  }
+}
+//package models;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class LocationTest {
+//	
+//	
+//	private Location one;
+//	private Location two;
+//	
+//	
+//	@Before
+//	public void setup()
+//	{
+//		one = new Location(23.3f,33.3f);
+//		two = new Location(34.4f,22.2f);
+//	}
+//	@After
+//	public void tearDown()
+//	{
+//		one = two = null;
+//	}
+//	
+//	
+//	
+//	
+//
+//	@Test
+//	  public void testCreate()
+//	  {
+//	    assertEquals ( 23.3f, one.latitude,0.01);
+//	    assertEquals ( 33.3f, one.longitude,0.01);
+//	  }
+//	  @Test
+//	  public void testIds()
+//	  {
+////	    Location one = new Location(23.3f, 33.3f);
+////	    Location two = new Location(34.4f, 22.2f);
+//	    assertNotEquals(one.id, two.id);
+//
+//	  }
+//
+//	  @Test
+//	  public void testToString()
+//	  {
+////	    Location one = new Location(23.3f, 33.3f);
+//	    assertEquals ("Location{2, 23.3, 33.3}", one.toString());
+//	  }
+//
+//}
